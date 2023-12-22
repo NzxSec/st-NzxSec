0bb1c86134035acf2e1a4da144213ca53321fd81
Kajan0v3zk1 <48596072+Extr3m4R4710@users.noreply.github.com>
2023-12-22 16:04:19 +0000

    Update config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "HackGen35:size=12";
+static char *font = "SourceCodePro-Medium:size=12";
 static int borderpx = 2;
 
 /*
